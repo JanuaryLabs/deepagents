4524a6a4f8c3a0b1c867ffb4ca3fe1d38f3a1e85
ezzabuzaid <ezza@kortext.com>
2025-12-16 12:38:45 +0300

    fix(text2sql): make synthesis named export

--- packages/text2sql/src/index.ts
+++ packages/text2sql/src/index.ts
@@ -10,28 +10,27 @@ export * from './lib/memory/memory.store.ts';
 export * from './lib/memory/sqlite.store.ts';
 export * from './lib/memory/store.ts';
 export * from './lib/sql.ts';
-export * from './lib/synthesis/extractors/index.ts';
 export * from './lib/teach/teachings.ts';
 
-if (import.meta.main) {
-  const { DatabaseSync } = await import('node:sqlite');
-  const sqliteClient = new DatabaseSync(
-    '/Users/ezzabuzaid/Downloads/Chinook.db',
-  );
-  const { Text2Sql } = await import('./lib/sql.ts');
-  const { InMemoryHistory } = await import('./lib/history/memory.history.ts');
-  const sqlite = await import('./lib/adapters/sqlite/index.ts');
+// if (import.meta.main) {
+//   const { DatabaseSync } = await import('node:sqlite');
+//   const sqliteClient = new DatabaseSync(
+//     '/Users/ezzabuzaid/Downloads/Chinook.db',
+//   );
+//   const { Text2Sql } = await import('./lib/sql.ts');
+//   const { InMemoryHistory } = await import('./lib/history/memory.history.ts');
+//   const sqlite = await import('./lib/adapters/sqlite/index.ts');
 
-  const adapter = new sqlite.Sqlite({
-    grounding: [sqlite.tables(), sqlite.constraints()],
-    execute: (sql) => sqliteClient.prepare(sql).all(),
-  });
+//   const adapter = new sqlite.Sqlite({
+//     grounding: [sqlite.tables(), sqlite.constraints()],
+//     execute: (sql) => sqliteClient.prepare(sql).all(),
+//   });
 
-  // console.dir(await text2sql.inspect(sqlQueryAgent), { depth: null });
-  // const sql = await text2sql.toSql(
-  //   'The top-selling products or categories each month last year given last record stored?. if the questions is wrong, show me full correct question I can ask.',
-  // );
-  // console.log('Generated SQL:');
-  // console.log(sql);
-  // await printer.readableStream(sql);
-}
+//   // console.dir(await text2sql.inspect(sqlQueryAgent), { depth: null });
+//   // const sql = await text2sql.toSql(
+//   //   'The top-selling products or categories each month last year given last record stored?. if the questions is wrong, show me full correct question I can ask.',
+//   // );
+//   // console.log('Generated SQL:');
+//   // console.log(sql);
+//   // await printer.readableStream(sql);
+// }
--- packages/text2sql/src/lib/synthesis/index.ts
+++ packages/text2sql/src/lib/synthesis/index.ts
@@ -2,3 +2,4 @@ export * from './decorators/index.ts';
 export * from './extractors/index.ts';
 export * from './synthesizers/index.ts';
 export * from './types.ts';
+export { type QuestionComplexity } from '../agents/question.agent.ts';
