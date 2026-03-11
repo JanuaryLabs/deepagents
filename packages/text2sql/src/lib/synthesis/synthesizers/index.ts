8c03ed17e3c6d803dcd63ddf342bb32d9372544c
ezzabuzaid <ezza@kortext.com>
2026-03-11 15:10:55 +0300

    refactor(text2sql): remove teaching agent

--- packages/text2sql/src/lib/synthesis/synthesizers/index.ts
+++ packages/text2sql/src/lib/synthesis/synthesizers/index.ts
@@ -1,6 +1,5 @@
-export * from './schema-synthesizer.ts';
 export * from './breadth-evolver.ts';
 export * from './depth-evolver.ts';
 export * from './persona-generator.ts';
-export * from './teachings-generator.ts';
+export * from './schema-synthesizer.ts';
 export * from './styles.ts';
